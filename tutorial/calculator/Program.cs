38a6e0c302c57f5754f55c0adaccb563e8073db5
disturbedhha <hughdarker.ins@gmail.com>
2024-05-06 16:10:57 -0500

    main

---
+++ tutorial/calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// sum
+int a = 20;
+int b = 10;
+
+int result = 0;
+
+result = a + b;
+Console.WriteLine("The result of the sum is: ");
+Console.Write(result);
---
+++ tutorial/hello/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = { 200, 100, 500, 10, 15, 9 };
+var sum = 0;
+
+foreach (int items in numbers){
+    sum += items;
+}
+
+Console.WriteLine(sum);
+
+
